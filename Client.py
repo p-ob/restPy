f33a418f4b696f05a3192f039b3325b27167fdca
Pat <patrick.obrien91@gmail.com>
2014-11-22 10:17:33 -0600

    Initial commit

---
+++ Client.py
@@ -0,0 +1,27 @@
+__author__ = 'Patrick O\'Brien'
+''' COPYRIGHT 2014
+    This file is part of restPy.
+
+    restPy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    restPy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with restPy.  If not, see <http://www.gnu.org/licenses/>.
+'''
+import requests
+
+
+class Client:
+    def __init__(self, base):
+        self.base = base
+        self.request = None
+
+    def execute(self, request):
+        self.request = request
---
+++ Request.py
@@ -0,0 +1,46 @@
+__author__ = 'Patrick O\'Brien'
+''' COPYRIGHT 2014
+    This file is part of restPy.
+
+    restPy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    restPy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with restPy.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
+class Request:
+    def __init__(self, method):
+        self.method = method
+        self.parameters = []
+
+    def add_url_parameter(self, name: str, value):
+        self.parameters.append(UrlParameter(name, value))
+
+    def add_query_parameter(self, name: str, value):
+        self.parameters.append(QueryParameter(name, value))
+
+
+class Parameter:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+
+class UrlParameter(Parameter):
+    def __init__(self, name: str, value):
+        super().__init__(name, value)
+
+
+class QueryParameter(Parameter):
+    def __init__(self, name, value):
+        super().__init__(name, value)
+

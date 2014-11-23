ddbb25e8934be8ba697ed56f75184102244175ef
Pat <patrick.obrien91@gmail.com>
2014-11-22 18:24:57 -0600

    Fixing errors

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 __author__ = 'Patrick O\'Brien'
 ''' COPYRIGHT 2014
-    This file is part of lolPy.
+    This file is part of restPy.
 
-    lolPy is free software: you can redistribute it and/or modify
+    restPy is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    lolPy is distributed in the hope that it will be useful,
+    restPy is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with lolPy.  If not, see <http://www.gnu.org/licenses/>.
+    along with restPy.  If not, see <http://www.gnu.org/licenses/>.
 '''
 from Client import Client
-from Request import Request, UrlParameter, QueryParameter
+from Request import Request
 
 c = Client('www.example.com')
 r = Request('api/{test}')

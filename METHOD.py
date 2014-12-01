e59c86b0b095b2232e0e042476e40c1618bf9b81
Patrick O'Brien <patrick.obrien91@gmail.com>
2014-12-01 11:37:47 -0600

    Create METHOD.py

---
+++ METHOD.py
@@ -0,0 +1,28 @@
+__author__ = 'Patrick O\'Brien'
+''' COPYRIGHT 2014
+    This file is part of restPy.
+
+    restPy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    restPy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with restPy.  If not, see <http://www.gnu.org/licenses/>.
+'''
+from enum import IntEnum
+
+
+class METHOD(IntEnum):
+    GET = 0
+    PUT = 1
+    POST = 2
+    DELETE = 3
+    # the following are not RESTful methods, but still included
+    HEAD = 4
+    OPTIONS = 5
